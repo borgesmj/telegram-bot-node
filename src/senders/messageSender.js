c7be96cf3e23d22f88c816fbd1fd198e5e2a72a2
borgesmj19 <borgesmj19@gmail.com>
2025-01-16 21:18:32 +0000

    enable markdown format

--- src/senders/messageSender.js
+++ src/senders/messageSender.js
@@ -1,6 +1,6 @@
 export default function messageSender(chatId, message, bot){
     try {
-        bot.sendMessage(chatId, message);
+        bot.sendMessage(chatId, message, {parse_mode: "markdown"});
     } catch (error) {
         console.log("no pudo enviarse el mensaje, intente de nuevo ", error)
     }
