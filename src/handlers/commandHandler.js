8f6e3a9ea4f06612e8fdefe38611ed8df682a9c9
borgesmj19 <borgesmj19@gmail.com>
2025-01-25 12:42:29 +0000

    decryted message

--- src/handlers/commandHandler.js
+++ src/handlers/commandHandler.js
@@ -7,7 +7,7 @@ import {
 } from "../database/databaseHandlers.js";
 import { changeName } from "./MessagesHandler.js";
 import sendMenu from "../senders/menuSender.js";
-import { generateUserIV } from "../helpers/encryptText.js";
+import { decryptText, generateUserIV } from "../helpers/encryptText.js";
 export default async function commandHandler(
   command,
     bot,
@@ -48,7 +48,7 @@ export default async function commandHandler(
         currentUser = await fetchCurrentUser(msg.from.id);
         await messageSender(
           chatId,
-          botReplies[10].replace("%username", currentUser.first_name),
+          botReplies[10].replace("%username", decryptText(currentUser.first_name)),
           bot
         );
       }
