d1cd200ae6d74f7459be8204dc4aefa3aa86c73f
borgesmj19 <borgesmj19@gmail.com>
2025-02-03 13:29:57 +0000

    checking user has username

--- src/handlers/commandHandler.js
+++ src/handlers/commandHandler.js
@@ -1,5 +1,6 @@
 import { fetchCurrentUser, fetchUsers } from "../database/databaseHandlers.js";
 import { decryptText, generateUserIV } from "../helpers/encryptText.js";
+import { botErrorMessages } from "../messages/botErrorMessages.js";
 import { botReplies } from "../messages/botMessages.js";
 
 export default async function commandHandler(
@@ -13,8 +14,8 @@ export default async function commandHandler(
   let userProfile = {};
   let inline_keyboard = [];
   let newTextMessage = "";
-  userManager.setUserProfile(chatId, await fetchCurrentUser(chatId))
-  currentUser = await userManager.getUserProfile(chatId)
+  userManager.setUserProfile(chatId, await fetchCurrentUser(chatId));
+  currentUser = await userManager.getUserProfile(chatId);
   switch (command.toLowerCase()) {
     case "start":
       const allUsers = await fetchUsers();
@@ -25,32 +26,42 @@ export default async function commandHandler(
         }
       });
       if (!isUser) {
-        const newUserProfile = {}
-        userManager.setUserStatus(chatId, "initial");
-        newUserProfile.telegram_id = chatId;
-        newUserProfile.first_name = msg.from.first_name;
-        newUserProfile.last_name = msg.from.last_name;
-        newUserProfile.telegram_username = msg.from.username;
-        newUserProfile.user_iv = await generateUserIV();
-        userManager.setNewUser(chatId, newUserProfile);
-        await messageSender.sendTextMessage(chatId, botReplies[0], []);
-        await new Promise((resolve) => setTimeout(resolve, 200));
-        inline_keyboard = [
-          [
-            { text: "Esta bien asi", callback_data: "not-edit-first-name-btn" },
-            { text: "Cambiarlo", callback_data: "edit-first-name-btn" },
-          ],
-        ];
-        newTextMessage = botReplies[1].replace(
-          "$username",
-          msg.from.first_name
-        );
-        await messageSender.sendTextMessage(
-          chatId,
-          newTextMessage,
-          inline_keyboard
-        );
-        userManager.setUserStatus(chatId, "initial");
+        if (!msg.from.username) {
+          await messageSender.sendTextMessage(chatId, botErrorMessages[2], []);
+          await new Promise(resolve => setTimeout(resolve, 500))
+          await messageSender.sendTextMessage(chatId, "Si necesitas mas información, utiliza el comando /ayuda", [])
+          return;
+        } else {
+          const newUserProfile = {};
+          userManager.setUserStatus(chatId, "initial");
+          newUserProfile.telegram_id = chatId;
+          newUserProfile.first_name = msg.from.first_name;
+          newUserProfile.last_name = msg.from.last_name;
+          newUserProfile.telegram_username = msg.from.username;
+          newUserProfile.user_iv = await generateUserIV();
+          userManager.setNewUser(chatId, newUserProfile);
+          await messageSender.sendTextMessage(chatId, botReplies[0], []);
+          await new Promise((resolve) => setTimeout(resolve, 200));
+          inline_keyboard = [
+            [
+              {
+                text: "Esta bien asi",
+                callback_data: "not-edit-first-name-btn",
+              },
+              { text: "Cambiarlo", callback_data: "edit-first-name-btn" },
+            ],
+          ];
+          newTextMessage = botReplies[1].replace(
+            "$username",
+            msg.from.first_name
+          );
+          await messageSender.sendTextMessage(
+            chatId,
+            newTextMessage,
+            inline_keyboard
+          );
+          userManager.setUserStatus(chatId, "initial");
+        }
       } else {
         userManager.setUserStatus(chatId, "initial");
         userProfile = await fetchCurrentUser(chatId);
@@ -62,12 +73,18 @@ export default async function commandHandler(
         );
         await messageSender.sendTextMessage(chatId, newTextMessage, []);
         await new Promise((resolve) => setTimeout(resolve, 200));
-        await messageSender.sendMenu(chatId)
+        await messageSender.sendMenu(chatId);
       }
 
       return;
-      case "menu":
-        messageSender.sendMenu(chatId, await currentUser.ROLE)
+    case "menu":
+      if (!msg.from.username) {
+        await messageSender.sendTextMessage(chatId, botErrorMessages[2], []);
+        await new Promise(resolve => setTimeout(resolve, 500))
+        await messageSender.sendTextMessage(chatId, "Si necesitas mas información, utiliza el comando /ayuda", [])
+        return;
+      }
+      messageSender.sendMenu(chatId, await currentUser.ROLE);
     default:
       break;
   }
